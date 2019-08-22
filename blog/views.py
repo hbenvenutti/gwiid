aa9837a052c46a84d60666a551e48adf0ec63c70
Huam Benvenutti <huambenvenutti@protonmail.com>
2019-08-22 15:07:45 -0300

    Create post_detail view

--- blog/views.py
+++ blog/views.py
@@ -22,6 +22,40 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+
+            return redirect('post_detail', pk=pk)
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+@login_required
+def post_remove(request, pk):
+
+    return
+
+
+@login_required
+def post_publish(request, pk):
+    return
+
+
+def post_comment(request, pk):
+    return
+
+
 # Futebol ============================================================
 def home(request):
     return render(request, 'blog/home.html')
